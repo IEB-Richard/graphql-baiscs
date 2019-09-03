10d15a8e6f41baa9e0a08d4b5d651d9dce8836ef
xiaoqiangliu <xiaoqiang.liu@yahoo.com>
2019-09-04 06:45:43 +0800

    complete subscription for comments

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -120,7 +120,7 @@ const Mutation = {
 
 		return post;
 	},
-	createComment(parent, args, { db }, info) {
+	createComment(parent, args, { db, pubsub }, info) {
 		const userExists = db.users.some((user) => user.id === args.data.author);
 		const postExists = db.posts.some((post) => post.id === args.data.post && post.published);
 
@@ -133,7 +133,8 @@ const Mutation = {
 			...args.data
 		};
 
-		db.comments.push(comment);
+        db.comments.push(comment);
+        pubsub.publish(`comment ${args.data.post}`, { comment })
 
 		return comment;
 	},
